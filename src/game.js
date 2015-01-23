85b00e63d23d908a6f3138d675882b180b532361
Matthias Lücke <ml@9bt.de>
2015-01-24 00:26:52 +0100

    menu state

--- src/game.js
+++ src/game.js
@@ -2,6 +2,8 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'Ultimate Drones');
 
 
 var playState = new Play();
+var menuState = new Menu();
 
 game.state.add('play', playState);
-game.state.start('play');
+game.state.add('menu', menuState);
+game.state.start('menu');
--- src/states/menu.js
+++ src/states/menu.js
@@ -1,6 +1,12 @@
 'use strict'
 
 function Menu(){
+    this.buttonStyle =  {font: '25px Arial', fill: '#ffffff', align: 'center'};
+    this.titleStyle =  {font: '65px Arial', fill: '#ffffff', align: 'center'};
+    
+    this.titleText = null;
+    this.playText = null;
+    this.highscoreText = null;
 
 }
 
@@ -10,9 +16,19 @@ Menu.prototype = {
     },
 
     create: function(){
-
+        this.titleText = this.game.add.text(this.game.world.centerX, 80, 'Ultimate Drones', this.titleStyle);
+        this.titleText.anchor.setTo(0.5, 0.5);
+        this.playText = this.game.add.text(this.game.world.centerX, this.game.world.centerY, 'Play', this.buttonStyle);
+        this.playText.anchor.setTo(0.5, 0.5);
+        this.highscoreText = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 30, 'Highscore', this.buttonStyle);
+        this.highscoreText.anchor.setTo(0.5, 0.5);
     },
+    
     update: function(){
+        this.game.input.onDown.addOnce(this.startGame, this);
+    },
 
+    startGame: function(){
+        this.game.state.start('play');
     }
 }
