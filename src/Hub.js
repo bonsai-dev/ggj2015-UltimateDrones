1c76668522e13eb663c28b3cbd143e58ad59f735
kasoban <git@kasoban.de>
2015-01-24 21:26:21 +0100

    WIP commit to merge

--- src/Hub.js
+++ src/Hub.js
@@ -10,9 +10,10 @@ function Hub(x, y, game, sprite){
     this.energyRegeneration = 0.1;
 
     this.reloadSlots = {
-      slot1: null,
-      slot2: null,
-      slot3: null
+        slot1: {x:x-10, y:y-10, worker: null},
+        slot2: {x:x-10, y:y, worker: null},
+        slot3: {x:x, y:y-10, worker: null},
+        slot4: {x:x, y:y-20, worker: null}
     };
 
 }
@@ -31,6 +32,8 @@ Hub.prototype =
     },
 
     assignWorker: function (worker) {
+        var freeSlot = this.getFreeSlot();
+        if(freeSlot === null)
         this.assignedWorkers.push(worker);
         worker.assignTask({
             type:"reloadFromHub",
@@ -51,6 +54,22 @@ Hub.prototype =
                 acceptEnergy(0);
             }
         }
+    },
+
+    getFreeSlot: function () {
+        if(slot1.worker === null) {
+            return slot1;
+        }
+        if(slot2.worker === null) {
+            return slot2;
+        }
+        if(slot3.worker === null) {
+            return slot3;
+        }
+        if(slot4.worker === null) {
+            return slot4;
+        }
+        return null;
     }
 
 };
