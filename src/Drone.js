4d03c31f3d66120b94d41463dd53e32a99038236
kasoban <git@kasoban.de>
2015-01-24 02:55:29 +0100

    test drone movements

--- src/Drone.js
+++ src/Drone.js
@@ -1,9 +1,20 @@
-function Drone(){
+function Drone(x, y, game){
 
     this.inventory = null;
+    this.x = x;
+    this.y = y;
+    this.game = game;
+    this.sprite = game.add.sprite(x,y, 'spacer');
+    this.moveSpeed = 0.2;
 
 }
 
-Drone.prototype.getInventory = function(){
-    return this.inventory;
-}
+
+
+Drone.prototype = {
+  move: function (mX, mY) {
+      var tweenSpeed = Math.sqrt((this.sprite.x-mX)*(this.sprite.x-mX)+(this.sprite.y-mY)+(this.sprite.y-mY))/this.moveSpeed;
+      console.log(tweenSpeed);
+      this.game.add.tween(this.sprite).to({x: mX, y: mY}, tweenSpeed, Phaser.Easing.Linear.None, true);
+  }
+};
--- src/ResourceDisplay.js
+++ src/ResourceDisplay.js
@@ -16,8 +16,11 @@ ResourceDisplay.prototype.create = function (resStorage) {
     this.resStorage = resStorage;
 
     this.res1Text = this.game.add.text(0, 0, this.initText, this.initStyle);
+    this.res1Text.fixedToCamera = true;
     this.res2Text = this.game.add.text(250, 0, this.initText, this.initStyle);
+    this.res2Text.fixedToCamera = true;
     this.res3Text = this.game.add.text(500, 0, this.initText, this.initStyle);
+    this.res3Text.fixedToCamera = true;
 };
 
 ResourceDisplay.prototype.update = function () {
