1e8e002fc999f8aa9dc4d0ce2590433b3f49804e
Matthias Lücke <ml@9bt.de>
2015-01-24 16:13:40 +0100

    hub work in progress

--- src/Hub.js
+++ src/Hub.js
@@ -1,6 +1,50 @@
-function Hub(){
+function Hub(x, y, game, sprite){
+    this.game = game;
+    this.sprite = this.game.add.sprite(x,y, sprite);
+    this.assignedWorkers = [];
 
+    this.text = this.game.add.text(x, y, this.storage, this.textStyle);
+
+    this.storedEnergy = 0;
+    this.maxStoredEnergy = 100;
+    this.energyRegeneration = 0.1;
+
+    this.reloadSlots = {
+      slot1: null,
+      slot2: null,
+      slot3: null
+    };
 
 }
 
-//Hub.prototype = new Building();
+Hub.prototype =
+{
+
+    tick: function(){
+
+        //regeneration
+        if(this.storedEnergy <= this.maxStoredEnergy){
+            this.storedEnergy += this.energyRegeneration;
+        }
+
+
+    },
+
+    assignWorker: function (worker) {
+        this.assignedWorkers.push(worker);
+        worker.assignTask({
+            type:"reloadFromHub",
+            position: {x:this.sprite.x, y:this.sprite.y},
+            slot:
+            delivery: this.acceptResources()
+        });
+    },
+
+    acceptResources: function() {
+        var that = this;
+        return function (resources) {
+            that.storage += resources;
+        }
+    }
+
+};
