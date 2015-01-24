a1572cc4c808f998d6ea4029ce94475cae107c01
Matthias Lücke <ml@9bt.de>
2015-01-24 14:04:11 +0100

    drone tweaks

--- src/Drone.js
+++ src/Drone.js
@@ -3,8 +3,13 @@ function Drone(x, y, game){
     this.inventory = null;
     this.maxInventory = 5;
     this.status = 'idle';
-    this.assignedBuilding = 0;
-    this.currentBuilding = 0;
+
+    this.textStyle =  {font: '25px Arial', fill: '#ffffff', align: 'left'};
+
+    this.energy = 100;
+    this.health = 100;
+
+    this.healthText = game.add.text(x, y, this.health, this.textStyle);
 
     this.game = game;
     this.sprite = game.add.sprite(x,y, 'spacer');
@@ -13,7 +18,8 @@ function Drone(x, y, game){
     this.sprite.body.customSeparateX = true;
     this.sprite.body.customSeparateY = true;
     this.sprite.body.immovable = true;
-    this.moveSpeed = 0.2;
+    this.moveSpeed = 0.15;
+    this.collectSpeed = 1;
     this.tween = this.game.add.tween(this.sprite);
     this.task = null;
 }
@@ -26,6 +32,7 @@ Drone.prototype = {
       //console.log(tweenSpeed);
 
       this.tween.stop(false);
+      this.tween = this.game.add.tween(this.healthText).to({x: mX, y: mY}, tweenSpeed, Phaser.Easing.Linear.None, true);
       this.tween = this.game.add.tween(this.sprite.body).to({x: mX, y: mY}, tweenSpeed, Phaser.Easing.Linear.None, true);
       this.tween.onComplete.add(callback, this);
 
@@ -36,16 +43,38 @@ Drone.prototype = {
           if(this.task.type === "collectResource") {
               if(this.status === 'idle') {
                   console.log("assigning move task");
+
+                  var moveX = 0;
+                  var moveY = 0;
+                  var clearZone = 50;
+
+                  while(moveX < this.task.dropOff.x + clearZone){
+                      moveX = randomBetween(this.task.area.x1, this.task.area.x2);
+                  }
+
+                  while(moveY < this.task.dropOff.y + clearZone){
+                      moveY = randomBetween(this.task.area.y1, this.task.area.y2);
+                  }
+
                   this.move(
-                      randomBetween(this.task.area.x1, this.task.area.x2),
-                      randomBetween(this.task.area.y1, this.task.area.y2),
+                      moveX,
+                      moveY,
                       function (that) {
                           var closuredTask = function() {
                               if(that.inventory < that.maxInventory) {
-                                  that.inventory += 0.5;
-                                  console.log("collecting", that.inventory);
-                                  console.log("returning to idle");
-                                  that.status = 'idle';
+
+                                  that.game.time.events.add(Phaser.Timer.SECOND * that.collectSpeed, function(){
+
+                                      that.inventory += 0.5;
+                                      console.log("collecting", that.inventory);
+                                      console.log("returning to idle");
+                                      that.status = 'idle';
+
+                                  }, this);
+
+
+
+
                               } else {
                                   console.log("inventory full, delivering");
                                   that.move(that.task.dropOff.x, that.task.dropOff.y, function() {
